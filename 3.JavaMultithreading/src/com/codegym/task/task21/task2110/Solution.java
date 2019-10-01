961b8c2115b583c3e70474da572e2b5cf275618a
Senned-001 <senned.mixed.by@gmail.com>
2019-10-01 11:51:56 +0300

    First commit

---
+++ 3.JavaMultithreading/src/com/codegym/task/task21/task2110/Solution.java
@@ -0,0 +1,41 @@
+package com.codegym.task.task21.task2110;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+/* 
+Refactoring methods
+
+*/
+public class Solution {
+    public static void writeZipEntriesToFile(String zipFileName, String outputFileName) {
+        Charset charset = StandardCharsets.UTF_8;
+        Path outputFilePath = Paths.get(outputFileName);
+
+
+
+        try (BufferedWriter writer =Files.newBufferedWriter(outputFilePath, charset);ZipFile zip =new ZipFile(zipFileName)){
+
+            String newLine = System.getProperty("line.separator");
+            for (Enumeration entries = zip.entries(); entries.hasMoreElements(); ) {
+                // Get the file name from the archive and write it to the output file
+                String zipEntryName = ((ZipEntry) entries.nextElement()).getName() + newLine;
+                writer.write(zipEntryName, 0, zipEntryName.length());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 3.JavaMultithreading/src/com/codegym/task/task21/task2112/DBConnectionManager.java
@@ -0,0 +1,7 @@
+package com.codegym.task.task21.task2112;
+
+public class DBConnectionManager {
+    public FakeConnection getFakeConnection() {
+        return new FakeConnection();
+    }
+}
