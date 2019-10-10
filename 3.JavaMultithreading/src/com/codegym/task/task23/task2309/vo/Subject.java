645eb7d78ce457994b44e5354bd6c3324f214ffd
Senned-001 <senned.mixed.by@gmail.com>
2019-10-10 13:22:00 +0300

    First commit

---
+++ 3.JavaMultithreading/src/com/codegym/task/task23/task2309/vo/Subject.java
@@ -0,0 +1,4 @@
+package com.codegym.task.task23.task2309.vo;
+
+public class Subject extends NamedItem{
+}
---
+++ 3.JavaMultithreading/src/com/codegym/task/task23/task2309/vo/Subscription.java
@@ -0,0 +1,4 @@
+package com.codegym.task.task23.task2309.vo;
+
+public class Subscription extends NamedItem{
+}
