b461f01c7dd7dcc8ac83894c96da7e77cbc0fc29
Senned-001 <senned.mixed.by@gmail.com>
2019-10-31 16:51:50 +0300

    First commit

---
+++ 3.JavaMultithreading/src/com/codegym/task/task30/task3008/client/Client.java
@@ -0,0 +1,50 @@
+package com.codegym.task.task30.task3008.client;
+
+import com.codegym.task.task30.task3008.Connection;
+import com.codegym.task.task30.task3008.ConsoleHelper;
+import com.codegym.task.task30.task3008.Message;
+import com.codegym.task.task30.task3008.MessageType;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Client {
+    protected Connection connection;
+    private volatile boolean clientConnected=false;
+
+    public class SocketThread extends Thread{
+
+    }
+
+    protected String getServerAddress(){
+        System.out.println("Enter server address:");
+        return ConsoleHelper.readString();
+
+    }
+
+    protected int getServerPort(){
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName(){
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole(){
+        return true;
+    }
+
+    protected SocketThread getSocketThread(){
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text){
+        try {
+            connection.send(new Message(MessageType.TEXT,text));
+        } catch (IOException e) {
+            clientConnected=false;
+        }
+    }
+}
+
