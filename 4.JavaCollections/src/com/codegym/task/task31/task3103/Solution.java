8a39a7cf5aa173c82b2f5df8a8ff8d17b7d4a062
Senned-001 <senned.mixed.by@gmail.com>
2019-11-11 15:33:03 +0300

    First commit

--- 4.JavaCollections/src/com/codegym/task/task31/task3103/Solution.java
+++ 4.JavaCollections/src/com/codegym/task/task31/task3103/Solution.java
@@ -12,16 +12,20 @@ Your own implementation
 */
 public class Solution {
     public static byte[] readBytes(String filename) throws IOException {
-        return null;
+        return Files.readAllBytes(Paths.get(filename));
     }
 
     public static List<String> readLines(String filename) throws IOException {
-        return null;
+        return Files.readAllLines(Paths.get(filename));
     }
 
     public static void writeBytes(String filename, byte[] bytes) throws IOException {
+        Files.write(Paths.get(filename),bytes);
     }
 
     public static void copy(String resourceFilename, String destinationFilename) throws IOException {
+        Files.copy(Paths.get(resourceFilename),Paths.get(destinationFilename));
+
     }
+
 }
