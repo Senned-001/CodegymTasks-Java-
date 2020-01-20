d5cc6418b314dcb11ad6535afda7766aa9455851
Senned-001 <senned.mixed.by@gmail.com>
2020-01-20 17:38:48 +0300

    First commit

--- 4.JavaCollections/src/com/codegym/task/task26/task2613/CurrencyManipulator.java
+++ 4.JavaCollections/src/com/codegym/task/task26/task2613/CurrencyManipulator.java
@@ -1,7 +1,8 @@
 package com.codegym.task.task26.task2613;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.codegym.task.task26.task2613.exception.InsufficientFundsException;
+
+import java.util.*;
 
 public class CurrencyManipulator {
     private String currencyCode;
@@ -37,5 +38,63 @@ public class CurrencyManipulator {
 
     public boolean hasMoney(){
         return denominations.size()>0;
+    }
+
+    public boolean isAmountAvailable(int expectedAmount){
+        return expectedAmount<=getTotalAmount();
+    }
+
+    public Map<Integer, Integer> withdrawAmount(int expectedAmount)throws InsufficientFundsException{
+        boolean found = false;
+        int totalsum = expectedAmount;
+        Map<Integer, Integer> copyOfDenominations = new HashMap<>();
+        copyOfDenominations.putAll(denominations);
+        Set<Integer> set=copyOfDenominations.keySet();
+        Map<Integer, Integer> result=new HashMap<>();
+        while(totalsum!=0){
+            int nominal = Collections.max(set);
+            if(copyOfDenominations.get(nominal)>0) {
+                if (nominal <= totalsum) {
+                    if(result.containsKey(nominal)){
+                        result.replace(nominal,result.get(nominal)+1);
+                    }else
+                        result.put(nominal,1);
+                    totalsum -= nominal;
+                    copyOfDenominations.replace(nominal,copyOfDenominations.get(nominal)-1);
+                }
+                else set.remove(nominal);
+            }else {
+                set.remove(nominal);
+            }
+            if(totalsum==0) found = true;
+            if(set.size()==0) break;
+        }
+
+        if(!found){                         //try make sum with same denomination (100+100+100 etc.)
+            copyOfDenominations.clear();
+            copyOfDenominations.putAll(denominations);
+            set=copyOfDenominations.keySet();
+            result=new HashMap<>();
+            for(Integer x:set){
+                if((expectedAmount%x==0)&&(copyOfDenominations.get(x)>=expectedAmount/x)){
+                    copyOfDenominations.replace(x,copyOfDenominations.get(x)-expectedAmount/x);
+                    result.put(x,expectedAmount/x);
+                    found = true;
+                }
+            }
+        }
+
+        if(!found) throw new InsufficientFundsException();
+
+        this.denominations.clear();
+        copyOfDenominations.entrySet().removeIf(e -> e.getValue() == 0);
+        this.denominations.putAll(copyOfDenominations);
+
+        /*System.out.println("Check denominations:");
+        for(Map.Entry<Integer,Integer> x:denominations.entrySet()){
+            System.out.println(x.getKey()+"-"+x.getValue());
+        }*/
+
+        return result;
     }
 }
--- 4.JavaCollections/src/com/codegym/task/task26/task2613/command/WithdrawCommand.java
+++ 4.JavaCollections/src/com/codegym/task/task26/task2613/command/WithdrawCommand.java
@@ -1,8 +1,48 @@
 package com.codegym.task.task26.task2613.command;
 
+import com.codegym.task.task26.task2613.ConsoleHelper;
+import com.codegym.task.task26.task2613.CurrencyManipulator;
+import com.codegym.task.task26.task2613.CurrencyManipulatorFactory;
+import com.codegym.task.task26.task2613.exception.InsufficientFundsException;
+import com.codegym.task.task26.task2613.exception.InterruptedOperationException;
+
+import java.util.*;
+
 class WithdrawCommand implements Command{
     @Override
-    public void execute() {
-
+    public void execute() throws InterruptedOperationException {
+        CurrencyManipulator currencyManipulator1 = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(ConsoleHelper.requestCurrencyCode());
+        while(true) {
+            ConsoleHelper.writeMessage("Enter a sum of withdraw:");
+            int expectedAmount;
+            try {
+                expectedAmount = Integer.parseInt(ConsoleHelper.readString());
+                if(expectedAmount<=0) throw new NumberFormatException();
+            } catch (NumberFormatException e) {
+                ConsoleHelper.writeMessage("Sum is not correct");
+                continue;
+            }
+            if(currencyManipulator1.isAmountAvailable(expectedAmount)){
+                Map<Integer, Integer> withdrawAmountMap;
+                try {
+                    withdrawAmountMap = currencyManipulator1.withdrawAmount(expectedAmount);
+                } catch (InsufficientFundsException e) {
+                    ConsoleHelper.writeMessage("There are insufficient banknotes, please enter other sum");
+                    continue;
+                }
+                Set<Integer> keys = withdrawAmountMap.keySet();
+                List<Integer> result = new ArrayList<>(keys);
+                Collections.sort(result);
+                Collections.reverse(result);
+                for(Integer x:result){
+                    ConsoleHelper.writeMessage("\t"+x+" - "+withdrawAmountMap.get(x));
+                }
+                ConsoleHelper.writeMessage("Transaction was successful");
+                break;
+            }
+            else {
+                ConsoleHelper.writeMessage("There are not enough money, try other sum");
+            }
+        }
     }
 }
