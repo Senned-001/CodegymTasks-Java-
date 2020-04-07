b6de0e7083768d07c2a05b460afff967c9177153
Senned-001 <senned.mixed.by@gmail.com>
2020-04-07 15:56:16 +0300

    First commit

--- 8.Games/src/com/codegym/games/moonlander/MoonLanderGame.java
+++ 8.Games/src/com/codegym/games/moonlander/MoonLanderGame.java
@@ -2,20 +2,32 @@ package com.codegym.games.moonlander;
 
 import com.codegym.engine.cell.*;
 
+import static com.codegym.engine.cell.Key.UP;
+
 public class MoonLanderGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
     private Rocket rocket;
+    private GameObject landscape;
+    private boolean isUpPressed;
+    private boolean isLeftPressed;
+    private boolean isRightPressed;
+
 
     @Override
     public void initialize() {
         setScreenSize(WIDTH, HEIGHT);
         createGame();
+        showGrid(false);
     }
 
     private void createGame(){
-        rocket= new Rocket(WIDTH/2,0);
+        isUpPressed = false;
+        isLeftPressed = false;
+        isRightPressed = false;
+        createGameObjects();
         drawScene();
+        setTurnTimer(50);
     }
 
     private void drawScene(){
@@ -25,6 +37,61 @@ public class MoonLanderGame extends Game {
             }
         }
         rocket.draw(this);
+        landscape.draw(this);
     }
 
+    private void createGameObjects(){
+        rocket = new Rocket(WIDTH/2,0);
+        landscape = new GameObject(0, 25, ShapeMatrix.LANDSCAPE);
+    }
+
+    @Override
+    public void onTurn(int step) {
+        rocket.move(isUpPressed,isLeftPressed,isRightPressed);
+        drawScene();
+    }
+
+    @Override
+    public void setCellColor(int x, int y, Color color) {
+        if(x>=0&&x<WIDTH&&y>=0&&y<HEIGHT)
+            super.setCellColor(x, y, color);
+    }
+
+    @Override
+    public void onKeyPress(Key key) {
+        switch (key){
+            case UP : {
+                isUpPressed = true;
+                break;
+            }
+            case RIGHT : {
+                isRightPressed = true;
+                isLeftPressed= false;
+                break;
+            }
+            case LEFT : {
+                isRightPressed = false;
+                isLeftPressed= true;
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onKeyReleased(Key key) {
+        switch (key){
+            case UP : {
+                isUpPressed = false;
+                break;
+            }
+            case RIGHT : {
+                isRightPressed = false;
+                break;
+            }
+            case LEFT : {
+                isLeftPressed= false;
+                break;
+            }
+        }
+    }
 }
--- 8.Games/src/com/codegym/games/moonlander/Rocket.java
+++ 8.Games/src/com/codegym/games/moonlander/Rocket.java
@@ -1,8 +1,46 @@
 package com.codegym.games.moonlander;
 
 public class Rocket extends GameObject {
+    private double speedY = 0;
+    private double boost = 0.05;
+    private double speedX = 0;
 
-    public Rocket(double x, double y) {
+
+        public Rocket(double x, double y) {
         super(x, y, ShapeMatrix.ROCKET);
+    }
+
+    public void move(boolean isUpPressed, boolean isLeftPressed, boolean isRightPressed){
+        if(isUpPressed) {
+            speedY-=boost;
+        } else {
+            speedY += boost;
+        }
+        this.y += speedY;
+
+        if(isLeftPressed){
+            speedX-=boost;
+            this.x += speedX;
+        }
+        if(isRightPressed){
+            speedX += boost;
+            this.x += speedX;
+        }
+        checkBorders();
+    }
+
+    private void checkBorders(){
+        if(this.x<0) {
+            this.x = 0;
+            speedX = 0;
+        }
+        if(this.x+this.width>MoonLanderGame.WIDTH) {
+            this.x = MoonLanderGame.WIDTH - this.width;
+            speedX = 0;
+        }
+        if(this.y<0) {
+            this.y = 0;
+            speedY = 0;
+        }
     }
 }
